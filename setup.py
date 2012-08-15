be22ae19380c84e0d201b98ee10bd8ced2309cb2
Johan Hake <hake.dev@gmail.com>
2012-08-15 13:49:01 +0200

    Added test and clean option to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,52 @@ __license__  = "GNU LGPL Version 3.0 or later"
 
 # System imports
 from distutils.core import setup
+from distutils.core import Command
 
+class clean(Command):
+    """
+    Cleans *.pyc so you should get the same copy as is in the VCS.
+    """
+
+    description = "remove build files"
+    user_options = [("all","a","the same")]
+
+    def initialize_options(self):
+        self.all = None
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import os
+        os.system("py.cleanup")
+        os.system("rm -f MANIFEST")
+        os.system("rm -rf build")
+        os.system("rm -rf dist")
+        os.system("rm -rf doc/_build")
+
+class run_tests(Command):
+    """
+    Runs all tests under the modelparameters/ folder
+    """
+
+    description = "run all tests"
+    user_options = []  # distutils complains if this is not here.
+
+    def __init__(self, *args):
+        self.args = args[0] # so we can pass it to other classes
+        Command.__init__(self, *args)
+
+    def initialize_options(self):  # distutils wants this
+        pass
+
+    def finalize_options(self):    # this too
+        pass
+
+    def run(self):
+        import os
+        os.system("python utils/run_tests.py")
+        
 # Version number
 major = 0
 minor = 1
@@ -21,4 +66,7 @@ A module providing parameter structure for physical modeling
       author = __author__.split("(")[0],
       author_email = __author__.split("(")[1][:-1],
       packages = ["modelparameters", "modelparameters.tests"],
+      cmdclass    = {'test': run_tests,
+                     'clean': clean,
+                     },
       )
---
+++ utils/run_tests.py
@@ -0,0 +1,40 @@
+"Run all tests"
+
+__author__ = "Johan Hake (hake.dev@gmail.com)"
+__date__ = "2012-08-15 -- 2012-08-15"
+__copyright__ = "Copyright (C) 2012 " + __author__
+__license__  = "GNU LGPL version 3.0"
+
+from modelparameters.commands import get_status_output
+
+import re, sys, os
+
+failed = []
+
+num_tests = 0
+timing = 0.0
+
+# Run tests
+for dirpath, dirnames, filenames in os.walk("modelparameters"):
+    if os.path.basename(dirpath) == "tests":
+        
+        print ""
+        print "Running tests in: %s" % dirpath
+        print "----------------------------------------------------------------------"
+        for test in filenames:
+            if "test_" not in test:
+                continue
+            fail, output = get_status_output("python %s" % os.path.join(dirpath, test))
+            num_tests += int(re.findall("Ran (\d+) tests", output)[0])
+            timing += float(re.findall("tests in ([\d\.]+)s", output)[0])
+            if fail:
+                failed.append(output)
+
+print
+print "----------------------------------------------------------------------"
+print "Ran %d tests in %.3fs" % (num_tests, timing)
+if failed:
+    for output in failed:
+        print output
+
+sys.exit(len(failed))
