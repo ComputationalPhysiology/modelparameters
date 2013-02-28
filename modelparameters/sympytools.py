33b1ee98b9de2ea5e91892f7af714d93b21f8383
Johan Hake <hake.dev@gmail.com>
2013-02-28 09:43:07 +0100

    Added one, two, three, four, five, and ten as sympified versions of the numbers. This is because Python do some funny things with:
      1/7
    
    It either turns it to 0 or to 0.14... So the preffered way to write that in gotran is:
    
      one/7
    
    Not pretty but usefull.

--- modelparameters/sympytools.py
+++ modelparameters/sympytools.py
@@ -319,4 +319,11 @@ sp_namespace.update((name, op) for name, op in relational.__dict__.items() \
 sp_namespace["pi"] = sp.numbers.pi
 sp_namespace["E"] = sp.numbers.E
 
+sp_namespace["one"] = sp.sympify(1)
+sp_namespace["two"] = sp.sympify(2)
+sp_namespace["three"] = sp.sympify(3)
+sp_namespace["four"] = sp.sympify(4)
+sp_namespace["five"] = sp.sympify(4)
+sp_namespace["ten"] = sp.sympify(10)
+
 __all__ = [_name for _name in globals().keys() if _name[0] != "_"]
